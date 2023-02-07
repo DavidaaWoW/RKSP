a368a1fcdf9a21d75b90bc40939ba957865f4fdc
DavidaaWoW <davidik.gegiya@gmail.com>
2023-02-07 18:22:20 +0300

    add pr1

---
+++ 1/ikbo-10-20-react-test-mirea/src/App.js
@@ -0,0 +1,29 @@
+import logo from './MIREA_Gerb_Colour.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mirea is the best university in the world!
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+               className="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+          </svg>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
